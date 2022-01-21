44522ff4f6602d979c8406720c741ec4ee4d3236
bl1ndy <ayoblindstone@gmail.com>
2022-01-21 14:23:21 +0300

    Init project

---
+++ lib/bl1ndy_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'bl1ndy_palindrome/version'
+
+module Bl1ndyPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bl1ndy_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Bl1ndyPalindrome
+  VERSION = '0.1.0'
+end
